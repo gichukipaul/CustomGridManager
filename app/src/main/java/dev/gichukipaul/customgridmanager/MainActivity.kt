0bdb31baf7d26f6911e4d04723c1d771cb2e7a2f
GichukiPaul <gichukipaul043@gmail.com>
2023-08-20 22:27:04 +0300

    Initial commit

---
+++ app/src/main/java/dev/gichukipaul/customgridmanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.gichukipaul.customgridmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
